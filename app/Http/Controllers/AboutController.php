e016aa01907ee44de6027601b0f349427406a6ed
KurniaDuta <dutaka2945@gmail.com>
2025-02-18 15:54:36 +0700

    Jobsheet 2 - Praktikum 2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() {
+        return 'Duta Kurnia Ardhani - 2341720139';
+    }
+}
